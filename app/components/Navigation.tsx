ebac4df615ac77fc95f026973847c1466091a715
Dearie <eburujacintha@gmail.com>
2024-12-08 08:16:09 +0100

    added tracker and removed css-in-js

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,15 +1,26 @@
 import Link from "next/link";
 
 export default function Navigation() {
-    return (
-      <ul>
-        <li><Link href="">Home</Link></li>
-        <li><Link href="">Learn</Link></li>
-        <li><Link href="">Wallet</Link></li>
-        <li><Link href="">Earn</Link></li>
-        <li><Link href="">Community</Link></li>
-        <li><Link href="">Profile</Link></li>
-      </ul>
-    );
-  }
-  
+  return (
+    <ul>
+      <li>
+        <Link href="">Home</Link>
+      </li>
+      <li>
+        <Link href="">Learn</Link>
+      </li>
+      <li>
+        <Link href="">Wallet</Link>
+      </li>
+      <li>
+        <Link href="">Earn</Link>
+      </li>
+      <li>
+        <Link href="">Community</Link>
+      </li>
+      <li>
+        <Link href="">Profile</Link>
+      </li>
+    </ul>
+  );
+}
