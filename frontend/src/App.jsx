6725298275b55c4b524451ebd459bbbd08fe44e2
Dearie <eburujacintha@gmail.com>
2025-05-16 15:31:12 +0400

    updated for route files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,24 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import './index.css'
+import SignUp from "./pages/SignUp";
 
 function App() {
-
-
   return (
-    <>
-  
-    </>
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        {/* Add more routes as needed */}
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/" element={<Home />} /> */}
+      </Routes>
+    </Router>
   )
 }
 
