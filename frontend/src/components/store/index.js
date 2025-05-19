61ed28a2a7080cd48ed0c02e37fb5032115325cc
Dearie <eburujacintha@gmail.com>
2025-05-19 12:46:41 +0400

    exported all zustand stores

---
+++ frontend/src/components/store/index.js
@@ -0,0 +1,13 @@
+
+
+// Export all Zustand stores
+export { default as useCourseStore } from './useCourseStore';
+export { default as useUserStore } from './useUserStore';
+
+// Future stores to be created:
+// export { default as useAuthStore } from './useAuthStore';
+// export { default as useUIStore } from './useUIStore';
+// export { default as useNotificationStore } from './useNotificationStore';
+
+// Usage example:
+// import { useCourseStore, useUserStore } from '../store';
