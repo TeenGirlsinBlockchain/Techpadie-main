40ab5accc037293f1427befb88ce01f32be29a14
Dearie <eburujacintha@gmail.com>
2025-05-19 12:46:05 +0400

    exported components

---
+++ frontend/src/components/hooks/index.js
@@ -0,0 +1,17 @@
+
+
+// Course-related hooks
+export { 
+  useCourses, 
+  useCourse, 
+  useCourseSearch 
+} from './courseHooks';
+
+// User-related hooks (to be created)
+// export { useUser, useAuth } from './userHooks';
+
+// Auth-related hooks (to be created)  
+// export { useLogin, useRegister, useLogout } from './authHooks';
+
+// Usage example:
+// import { useCourses, useCourse } from '../hooks';
